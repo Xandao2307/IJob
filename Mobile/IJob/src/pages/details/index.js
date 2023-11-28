bf510ce1624b0e7cf62e2941e8833e0bdf203cae
Alexandre Souza <allexandre.souza2307@gmail.com>
2023-11-28 02:50:53 -0300

    mudanças na página details

--- Mobile/IJob/src/pages/details/index.js
+++ Mobile/IJob/src/pages/details/index.js
@@ -43,76 +43,93 @@ export default function Details({ route, navigation }) {
     );
   }
 
-  return (
-   <View style={{backgroundColor:'#F1F6F9'}}> 
-    <HeaderComponent
-      title='Detalhes do Prestador'
-    />
-      <View style = {[styles.container, {flex:0, marginTop:30, backgroundColor:'#F1F6F9'}]}>
-        <View style = {{flexDirection: 'row'}}>
-          <AvatarComponent
-            size={140}
-          />
-            <View style={{flexDirection:'column', paddingTop:15}}>
-              <Text style={[styles.formTitle,{fontSize:18, paddingBottom:12, margin:0,marginLeft:0, alignSelf:"flex-start"}]}>{user.name}</Text>
-              <Text style={[styles.formTitle,{fontSize:18, paddingBottom:0, margin:0, marginLeft:0, alignSelf:'flex-start'}]}>Serviços disponíveis:</Text>
-              <Text style={{fontSize:16, color:'#14274E'}}>{services}</Text>
-              <TouchableOpacity 
-                style={[styles.formButton,{width:138, elevation:5 ,height:30, padding:0, paddingTop:3, alignSelf:'flex-end'}]}
-              >
-                <Text style={{color:'white', fontSize:15,textAlign:'center', fontWeight:'bold'}} onPress={()=> Alert.alert("Descrição",user.description)} >Mais Informações</Text>
-              </TouchableOpacity>
+  const AvaliacoesCard = () => {
+    // Exemplo de dados de avaliação
+    const avaliacoes = [
+      {
+        nome: 'Vanila',
+        data: '23/07/2023',
+        avaliacao: 4,
+        comentario: 'Atrasou na chegada mas o serviço é excelente.',
+      },  {
+        nome: 'Vanila',
+        data: '23/07/2023',
+        avaliacao: 4,
+        comentario: 'Atrasou na chegada mas o serviço é excelente.',
+      },
+      // Adicione mais avaliações conforme necessário
+    ];
+  
+    return (
+      <Card containerStyle={{ width: 320, elevation: 5, backgroundColor: 'white', borderRadius: 10, marginBottom: 20 }}>
+        <Card.Title style={{ alignSelf: 'flex-start', fontSize: 18, color: '#14274E' }}>Avaliações:</Card.Title>
+        <Card.Divider />
+
+        {avaliacoes.map((item, index) => (
+          <View key={index}>
+            <View style={{ flexDirection: 'row' }}>
+              <Text style={{ fontSize: 16, color: '#14274E', marginRight: 6 }}>{item.nome}</Text>
+              <Text style={{ fontSize: 16, color: '#14274E' }}>{item.data}</Text>
+              <AirbnbRating size={14} showRating={false} isDisabled={true} defaultRating={item.avaliacao} count={5} />
             </View>
-        </View>
+            <Text style={{ fontSize: 14, color: '#14274E' }}>{item.comentario}</Text>
+            <Card.Divider />
 
-        <Card containerStyle={{ width: 320, elevation: 5, backgroundColor: 'white', borderRadius: 10 }}>
-          <Card.Title style={{ alignSelf: 'flex-start', fontSize: 18, fontWeight: 'bold', color: '#14274E' }}>
-            Vitrine do Prestador:
-          </Card.Title>
-          <FlatList
-            data={imageList}
-            keyExtractor={(item, index) => index.toString()}
-            horizontal={true}
-            renderItem={({ item }) => (
-              <Image
-                style={{ height: 150, width: 160, borderRadius: 8, marginVertical: 5, marginRight:10}}
-                source={{ uri: item }}
-              />
-            )}
-          />
-        </Card>
-        
-        <Card containerStyle={{width:320, elevation:5 , backgroundColor:'white', borderRadius:10}}>
-          <Card.Title style={{alignSelf:'flex-start',fontSize:18, color:'#14274E'}}>Avaliações:</Card.Title>
-          <View>
-          <Card.Divider />
-
-            <View style={{flexDirection: 'row'}}>
-              <Text style={{fontSize:16, color:'#14274E', marginRight:6}}>Vanila</Text>
-              <Text style={{fontSize:16, color:'#14274E'}}>23/07/2023</Text>
-
-              <AirbnbRating 
-              size={14}
-              showRating={false} 
-              isDisabled={true} 
-              defaultRating={4}
-              count={5}
-              />
-           </View>
-           <Text style={{fontSize:14, color:'#14274E'}}>Atrasou na chegada mas o serviço é excelente.</Text>
-           
           </View>
-        </Card>
+        ))}
+      </Card>
+    );
+  };
 
-        <TouchableOpacity onPress={()=>navigation.navigate('Conversation')} style={[styles.formButton, {backgroundColor:'#14274E', elevation:5 , marginBottom:0}]}>
-          <Text style={styles.textButton}>Iniciar Chat</Text>
-        </TouchableOpacity>
+  return (
+    <View style={{ flex: 1, backgroundColor: '#F1F6F9' }}>
+      <HeaderComponent title="Detalhes do Prestador" />
+      <View style={{ flex: 1 }}>
+        <ScrollView>
+          <View style={[styles.container, { marginTop: 30, backgroundColor: '#F1F6F9' }]}>
+            <View style={{ flexDirection: 'row' }}>
+              <AvatarComponent size={140} />
+              <View style={{ flexDirection: 'column', paddingTop: 15 }}>
+                <Text style={[styles.formTitle, { fontSize: 18, paddingBottom: 12, margin: 0, marginLeft: 0, alignSelf: 'flex-start' }]}>{user.name}</Text>
+                <Text style={[styles.formTitle, { fontSize: 18, paddingBottom: 0, margin: 0, marginLeft: 0, alignSelf: 'flex-start' }]}>Serviços disponíveis:</Text>
+                <Text style={{ fontSize: 16, color: '#14274E' }}>{services}</Text>
+                <TouchableOpacity
+                  style={[styles.formButton, { width: 138, elevation: 5, height: 30, padding: 0, paddingTop: 3, alignSelf: 'flex-end' }]}
+                >
+                  <Text style={{ color: 'white', fontSize: 15, textAlign: 'center', fontWeight: 'bold' }} onPress={() => Alert.alert("Descrição", user.description)}>Mais Informações</Text>
+                </TouchableOpacity>
+              </View>
+            </View>
 
-        <TouchableOpacity onPress={()=> navigation.navigate('Scheduling')} style={[styles.formButton, {backgroundColor:'#14274E', elevation:10 }]}>
-          <Text style={styles.textButton}>Agendar</Text>
-        </TouchableOpacity>
+            <Card containerStyle={{ width: 320, elevation: 5, backgroundColor: 'white', borderRadius: 10 }}>
+              <Card.Title style={{ alignSelf: 'flex-start', fontSize: 18, fontWeight: 'bold', color: '#14274E' }}>
+                Vitrine do Prestador:
+              </Card.Title>
+              <FlatList
+                data={imageList}
+                keyExtractor={(item, index) => index.toString()}
+                horizontal={true}
+                renderItem={({ item }) => (
+                  <Image
+                    style={{ height: 150, width: 160, borderRadius: 8, marginVertical: 5, marginRight: 10 }}
+                    source={{ uri: item }}
+                  />
+                )}
+              />
+            </Card>
+
+            <AvaliacoesCard />
+
+            <TouchableOpacity onPress={() => navigation.navigate('Conversation')} style={[styles.formButton, { backgroundColor: '#14274E', elevation: 5, marginBottom: 0 }]}>
+              <Text style={styles.textButton}>Iniciar Chat</Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity onPress={() => navigation.navigate('Scheduling')} style={[styles.formButton, { backgroundColor: '#14274E', elevation: 10 }]}>
+              <Text style={styles.textButton}>Agendar</Text>
+            </TouchableOpacity>
+          </View>
+        </ScrollView>
       </View>
     </View>
-  )
+  );
 }
-
